cbaa695d8554b583543e10a0680c85a931cdaef5
Mauricio Figueroa <mauricio.figueroa@gilbarco.com>
2017-07-25 16:47:32 -0300

    New WPF

---
+++ QualityScenariosManager/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QualityScenariosManager
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			Close();
+		}
+
+		private void Label_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+
+		private void CreateTSButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (ContentPanel.Children.Count > 0)
+			{
+				ContentPanel.Children.Clear();
+			}
+			ContentPanel.Children.Add(new TestSuiteCreation());
+			TitleLabel.Content = "Test suite creation";
+		}
+	}
+}
